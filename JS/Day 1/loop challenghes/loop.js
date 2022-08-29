ca79921d21361750ffe6f9f2ac3305ea4fdded9d
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2022-08-29 15:42:56 +0300

    submit

---
+++ JS/Day 1/loop challenghes/loop.js
@@ -0,0 +1,26 @@
+for (var num1 = 1; num1 <= 20; num1++) {                                           
+    if (num1 % 2 == 1) {
+        console.log(num1);
+    }
+}
+
+for (var num2 = 1; num2 > 0; num2--) {
+    if (num2 % 3 == 0) {
+        console.log(num2);
+    }
+}
+
+for (var num3 = 4; num3 >= -3.5; num3 -= 1.5) {
+    console.log(num3);
+}
+var sum = 0;
+for (var num4 = 1; num4 <= 100; num4++) {
+    sum += num4;
+}
+console.log(sum);
+
+var product = 1;
+for (var num5 = 1; num5 <= 12; num5++) {
+    product *= num5;
+}
+console.log(product);
