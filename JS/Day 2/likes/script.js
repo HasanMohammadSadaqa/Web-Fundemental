442043dc56b174d804186553262c49bffb84dd63
HasanMohammadSadaqa <hasansadaqa55@gmail.com>
2022-09-03 19:13:51 +0300

    submit

--- JS/Day 2/likes/script.js
+++ JS/Day 2/likes/script.js
@@ -1,15 +1,38 @@
-function increaselikes(){
-    var x=document.querySelector("#Number");
-    var z=document.querySelector("#Numbers");
-    var H=parseInt(z.innerHTML);
+// function increaselikes(){
+//     var x=document.querySelector("#Number");
+//     var z=document.querySelector("#Numbers");
+//     var H=parseInt(z.innerHTML);
+//     var y=parseInt(x.innerHTML);
+//     y++;
+//     H++
+//     x.innerText=y;
+//     z.innerText=H;
+
+// }
+function like1(){
+    var x=document.getElementById("like1")
     var y=parseInt(x.innerHTML);
     y++;
-    H++
-    x.innerText=y;
-    z.innerText=H;
+    x.innerHTML=y+ " likes";
 
 }
 
+
+function like2(){
+    var x=document.getElementById("like2")
+    var y=parseInt(x.innerHTML);
+    y++;
+    x.innerHTML=y+ " likes";
+
+}
+
+function like3(){
+    var x=document.getElementById("like3")
+    var y=parseInt(x.innerHTML);
+    y++;
+    x.innerHTML=y+ " likes";
+
+}
 // var x=document.querySelectorAll("#Number");
 // var y=[];
 // function increaselikes(){
